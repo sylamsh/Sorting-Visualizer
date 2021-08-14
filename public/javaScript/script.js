a555427d6f19d0cef4b7096445b4d38374456a39
Sylamsh <amrutasrisvara@gmail.com>
2021-08-14 23:37:42 +0530

    'drawing-on-all-canvases'

--- public/javaScript/script.js
+++ public/javaScript/script.js
@@ -1,5 +1,7 @@
-var numOfBars = 40;
+const canvas = document.querySelectorAll('canvas');
+const thumbnail = document.querySelector('.containThumbnail');
 
+var numOfBars = 40;
 
 const shuffledArrayInRange = (bottom = 1, top = numOfBars) => {
     const arr = [];
@@ -21,7 +23,7 @@ const bubbleSort = (array, onAction) => {
     return array;
 };
 
-function ArrayMember(x, y, width, height, color = "gray") {
+function ArrayMember(x, y, width, height, ctx, color = "gray") {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -52,28 +54,74 @@ function ArrayMember(x, y, width, height, color = "gray") {
 }
 
 const resetBars = () => {
-    canvas.height = '170';
-    canvas.width = thumbnail.offsetWidth - '50';
 
-    var barWidth = (canvas.width - numOfBars) / numOfBars;
-    var posConst = (canvas.width - numOfBars * 0.69) / numOfBars;
-    var barMaxHeight = canvas.height/numOfBars;
+    for(var i=0; i<canvas.length; i++)
+    {
+        canvas[i].height = '170';
+        canvas[i].width = thumbnail.offsetWidth - '50';
+    }
+   
+    const ctxArray = [...canvas].map(cvs => cvs.getContext('2d'));
+
+    var barWidth = (canvas[0].width - numOfBars) / numOfBars;
+    var posConst = (canvas[0].width - numOfBars * 0.75) / numOfBars;
+    var barMaxHeight = canvas[0].height/numOfBars;
     
     const randomArray = shuffledArrayInRange();
-    const arrayMembers = randomArray.map((v, i) => {
-        return new ArrayMember(posConst * i + i, 0, barWidth, v * barMaxHeight);
-    });
+    for(var j=0; j<canvas.length; j++){
+        const arrayMembers = randomArray.map((v, i) => {
+            return new ArrayMember(posConst * i + i, 0, barWidth, v * barMaxHeight, ctxArray[j]);
+        });
+        const drawAllBars = () => arrayMembers.forEach((m) => m.draw());
+        drawAllBars()
+    }
     
-    const drawAll = () => arrayMembers.forEach((m) => m.draw());
-    drawAll()
 }
-    
-const canvas = document.querySelector('canvas');
-const thumbnail = document.querySelector('.containThumbnail');
-const ctx = canvas.getContext("2d");
 
 window.addEventListener('resize', resetBars);
 window.addEventListener('load', resetBars);
 
 
 
+// const ACTIONS = {
+//     SORT: "SORT",
+//     COMPARE: "COMPARE",
+//     SWAP: "SWAP",
+// };
+
+// // ---- what to do on each action ----
+
+// const actionsMap = {
+//   [ACTIONS.SORT]: (action, members) => members[action.data].sorted(),
+//   [ACTIONS.SWAP]: (action, members) => {
+//     const [i, j] = action.data;
+//     let tmp = members[i].getValue();
+//     members[i].setValue(members[j].getValue(), "red");
+//     members[j].setValue(tmp, "yellow");
+//   },
+//   [ACTIONS.COMPARE]: (action, members) => {
+//     const [i, j] = action.data;
+//     members[i].setColor("blue");
+//     members[j].setColor("blue");
+//   },
+// };
+
+// bubbleSort(randomArray, (action) => {
+//   actionsMap[action.type](action, arrayMembers);
+//   ctx.clearRect(0, 0, innerWidth, innerHeight);
+//   drawAll();
+// })
+
+
+// let ticks = 0;
+// const speed = 50;
+
+// bubbleSort(randomArray, (action) => {
+//   ticks++;
+//   setTimeout(() => {
+//     actionsMap[action.type](action, arrayMembers);
+//     ctx.clearRect(0, 0, innerWidth, innerHeight);
+//     drawAll(arrayMembers);
+//     arrayMembers.forEach((m) => m.resetColor());
+//   }, ticks * speed);
+// });
